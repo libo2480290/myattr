186d906ca69c10fa5b4e86b2d34c7b7fbc45c0f5
libo <libo1@haizhi.com>
2016-07-13 16:17:58 +0800

    Initial commit

---
+++ app/src/main/java/com/example/libo/myapplicationattr/MyView.java
@@ -0,0 +1,26 @@
+package com.example.libo.myapplicationattr;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+/**
+ * Created by libo on 16/7/13.
+ */
+public class MyView extends TextView {
+
+    private String mString = "Welcome to Kesion's blog";
+
+    public MyView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.MyView);
+        int textColor = a.getColor(R.styleable.MyView_textColor,
+                0XFFFFFFFF);
+        float textSize = a.getDimension(R.styleable.MyView_textSize, 36);
+        mString = a.getString(R.styleable.MyView_title);
+        setText(mString);
+        setTextSize(textSize);
+        setTextColor(textColor);
+    }
+}
